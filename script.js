4198c7c5be8465a9ab80eb62eb1400d55ae98a56
Toure Lassana <l.toure910@laposte.net>
2024-09-20 15:24:55 +0200

    test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-fetch('https://whatyearisit-backend-8qlfa3lgp-lassanas-projects.vercel.app')
+fetch('https://whatyearisit-backend-8qlfa3lgp-lassanas-projects.vercel.app/year')
  .then(response => response.json())
  .then(data => {
    document.querySelector('#year').textContent = data.current;

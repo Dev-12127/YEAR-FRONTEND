7d5f7c06c9c6c51c70dadfc06a576c4e30bb5386
Toure Lassana <l.toure910@laposte.net>
2024-09-20 14:52:23 +0200

    test

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
-fetch('whatyearisit-backend-tau-red.vercel.app')
+fetch('https://whatyearisit-backend-hgyhxyc2x-lassanas-projects.vercel.app')
  .then(response => response.json())
  .then(data => {
    document.querySelector('#year').textContent = data.current;
  });
 
+ 
